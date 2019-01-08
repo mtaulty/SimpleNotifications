f481db92fad8510647f7fa6d3a2e7a9427d310a5
Mike Taulty <mtaulty@microsoft.com>
2019-01-08 11:18:28 +0000

    First, hopefully only version

---
+++ SimpleNotifications/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace SimpleNotifications
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        void OnRegister()
+        {
+            if (!HasTask)
+            {
+                var builder = new BackgroundTaskBuilder();
+                builder.Name = "Timer Task";
+
+                // We ask for a 15 minute timer trigger which is the minimum.
+                builder.SetTrigger(new TimeTrigger(15, false));
+                builder.Register();
+            }
+        }
+        void OnUnregister()
+        {
+            if (HasTask)
+            {
+                RegisteredTask.Unregister(false);
+            }
+        }
+        static IBackgroundTaskRegistration RegisteredTask =>
+            BackgroundTaskRegistration.AllTasks.Values.FirstOrDefault();
+        
+        static bool HasTask => 
+            RegisteredTask != null;
+    }
+}
